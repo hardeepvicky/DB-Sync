024f9d57e850a845ac42d40cab342b9b42b32884
Hardeepe Singh <hardeep@techformation.co.in>
2017-03-22 10:42:46 +0530

    minor changes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('memory_limit', '128M');
+ini_set('memory_limit', '512M');
 ini_set('max_execution_time', 60 * 60 * 30);   
             
 require_once './php/include/functions.php';
@@ -15,6 +15,8 @@ if (!FileUtility::createFolder(BASE_PATH . "developers/"))
 {
 	die("failed to create path " . BASE_PATH . "developers/");
 }
+
+chmod(BASE_PATH, 0777);
 
 $mysql = new Mysql(config::$database['server'], config::$database['username'], config::$database['password'], config::$database['database']);
 
